3d4569c1e0ad354a20a5931cbd4ba04cb2e53c03
Felipe Benincaza <fbenincaza@gmail.com>
2023-03-17 18:10:09 -0300

    - start projeto;

---
+++ app/src/main/java/com/benincaza/projetointegracoeskotlin/CreateAccount.kt
@@ -0,0 +1,157 @@
+package com.benincaza.projetointegracoeskotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
+class CreateAccount : AppCompatActivity() {
+
+    lateinit var edtEmail: EditText
+    lateinit var edtPassword: EditText
+    lateinit var edtConfirmPassword: EditText
+    lateinit var createAccountInputArray: Array<EditText>
+
+    val Req_Code:Int=123456;
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+        supportActionBar?.hide()
+
+        FirebaseApp.initializeApp(this)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        edtEmail = findViewById<EditText>(R.id.edt_email)
+        edtPassword = findViewById<EditText>(R.id.edt_password)
+        edtConfirmPassword = findViewById<EditText>(R.id.edt_confirm_password)
+
+        createAccountInputArray = arrayOf(edtEmail, edtPassword, edtConfirmPassword)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        findViewById<View>(R.id.txt_login_view).setOnClickListener{
+            val activity = Intent(this, LoginScreen::class.java)
+            startActivity(activity)
+        }
+
+        findViewById<View>(R.id.btn_signin).setOnClickListener {
+            Toast.makeText(this, R.string.registro_com_google, Toast.LENGTH_SHORT).show()
+            signInGoogle()
+        }
+
+        findViewById<View>(R.id.btn_create_account).setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signInGoogle(){
+        val signIntent: Intent = mGoogleSignInClient.signInIntent;
+        startActivityForResult(signIntent, Req_Code);
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == Req_Code){
+            val result = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleResult(result);
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
+        try{
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java);
+            Toast.makeText(this, R.string.login_success, Toast.LENGTH_SHORT).show();
+            if(account != null){
+                UpdateUser(account)
+            }
+        }catch (e: ApiException){
+            println(e)
+            Toast.makeText(this, R.string.login_failed, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private fun UpdateUser(account: GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {task ->
+            if(task.isSuccessful){
+                val intent = Intent(this, MainActivity::class.java);
+                startActivity(intent);
+                finish()
+            }
+        }
+    }
+
+    private fun verifyFields(): String{
+        if (edtPassword.text.toString().trim().isEmpty() && edtEmail.text.toString().trim().isEmpty() && edtConfirmPassword.text.toString().trim().isEmpty())
+            return getString(R.string.fill_all_fields)
+
+        if(edtPassword.text.toString().trim() != edtConfirmPassword.text.toString().trim())
+            return getString(R.string.passwords_not_match)
+
+        if(edtPassword.text.toString().trim().length < 6)
+            return getString(R.string.password_inadequate_length)
+
+        return ""
+    }
+
+    private fun signIn() {
+        val msg = verifyFields()
+        if (msg.isEmpty()) {
+
+            val userEmail = edtEmail.text.toString().trim()
+            val userPassword = edtPassword.text.toString().trim()
+
+            firebaseAuth.createUserWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        sendEmailVerification()
+                        Toast.makeText(this, R.string.user_created_success, Toast.LENGTH_SHORT).show()
+                        finish()
+                    } else {
+                        val exception = task.exception;
+                        if (exception is FirebaseAuthException && exception.errorCode == "ERROR_EMAIL_ALREADY_IN_USE") {
+                            Toast.makeText(this, R.string.email_registered, Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this, R.string.erro_create_user, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+        } else {
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun sendEmailVerification(){
+        val firebaseUser: FirebaseUser? = firebaseAuth.currentUser
+
+        firebaseUser?.let {
+            it.sendEmailVerification().addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    Toast.makeText(this, R.string.email_success_sent, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/benincaza/projetointegracoeskotlin/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.benincaza.projetointegracoeskotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.SparseBooleanArray
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class MainActivity : AppCompatActivity() {
+
+    private val PRODUCTS = "produtos"
+    lateinit var mGoogleSignClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignClient = GoogleSignIn.getClient(this, gso)
+
+        val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+        val firebaseUser: FirebaseUser? = firebaseAuth.currentUser
+
+        if(firebaseUser == null){
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
+        }
+
+        val listViewTasks = findViewById<ListView>(R.id.listViewTasks)
+        val edtProduct = findViewById<EditText>(R.id.edt_product)
+
+        val itemList = ListaComprasPreferences(this).getString(PRODUCTS)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, itemList)
+
+        listViewTasks.adapter = adapter
+        adapter.notifyDataSetChanged()
+
+        findViewById<View>(R.id.btn_add).setOnClickListener {
+            itemList.add(edtProduct.text.toString());
+            listViewTasks.adapter = adapter
+            adapter.notifyDataSetChanged()
+
+            ListaComprasPreferences(this).storeString(PRODUCTS, itemList)
+            edtProduct.text.clear()
+        }
+
+        findViewById<View>(R.id.btn_delete).setOnClickListener {
+            val position: SparseBooleanArray = listViewTasks.checkedItemPositions
+            val count = listViewTasks.count
+            var item = count - 1
+            while(item >= 0){
+                if(position.get(item)){
+                    adapter.remove(itemList.get(item))
+                }
+                item--
+            }
+
+            ListaComprasPreferences(this).storeString(PRODUCTS, itemList)
+            position.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        findViewById<View>(R.id.btn_clear).setOnClickListener {
+            itemList.clear()
+            ListaComprasPreferences(this).storeString(PRODUCTS, itemList)
+            adapter.notifyDataSetChanged()
+        }
+
+        findViewById<View>(R.id.logout).setOnClickListener{
+            firebaseAuth.signOut()
+            mGoogleSignClient.signOut()
+
+            startActivity(Intent(this, LoginScreen::class.java))
+        }
+    }
+}
