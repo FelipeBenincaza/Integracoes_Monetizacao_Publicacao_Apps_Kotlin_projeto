6f9e19e3ea2057e419928b4925906b90fd35eddc
Felipe Benincaza <fbenincaza@gmail.com>
2023-03-25 17:27:05 -0300

    - email invalido;

--- app/src/main/java/com/benincaza/projetointegracoeskotlin/ValidateAuthentication.kt
+++ app/src/main/java/com/benincaza/projetointegracoeskotlin/ValidateAuthentication.kt
@@ -2,6 +2,8 @@ package com.benincaza.projetointegracoeskotlin
 
 import android.content.Context
 import android.widget.EditText
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class ValidateAuthentication(val context: Context) {
 
@@ -9,6 +11,13 @@ class ValidateAuthentication(val context: Context) {
     fun validaCampoEmail(email: EditText) {
         if (email.text.toString().isEmpty())
             throw ValidateAuthenticationException(context.getString(R.string.campo_email_vazio))
+
+        val expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"
+        val pattern: Pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
+        val matcher: Matcher = pattern.matcher(email.text.toString())
+        if (!matcher.matches()) {
+            throw ValidateAuthenticationException(context.getString(R.string.email_invalido))
+        }
     }
 
     @Throws(ValidateAuthenticationException::class)
