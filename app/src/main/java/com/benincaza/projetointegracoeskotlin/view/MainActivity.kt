ec095afecb350561a3750f5810cd3672bb79cd62
Felipe Benincaza <fbenincaza@gmail.com>
2023-03-31 20:37:40 -0300

    - criação do cardView;

--- app/src/main/java/com/benincaza/projetointegracoeskotlin/view/MainActivity.kt
+++ app/src/main/java/com/benincaza/projetointegracoeskotlin/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.benincaza.projetointegracoeskotlin.view
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
@@ -9,6 +10,7 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import com.benincaza.projetointegracoeskotlin.R
 import com.benincaza.projetointegracoeskotlin.databinding.ActivityMainBinding
 import com.benincaza.projetointegracoeskotlin.fragments.WeatherFragments
@@ -21,6 +23,10 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.io.File
+import java.io.FileOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -53,6 +59,34 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        if(firebaseUser != null){
+            val displayName = firebaseUser.displayName
+            val email = firebaseUser.email
+            val photoUrl = firebaseUser.photoUrl
+
+            if(displayName.toString() != "") {
+                val nameSplit = displayName.toString().split(" ")
+                if(nameSplit.size > 1){
+                    binding.txtNome.setText(nameSplit[0])
+                    binding.txtSobrenome.setText(nameSplit[1])
+                }else{
+                    binding.txtNome.setText(displayName.toString())
+                    binding.txtSobrenome.isVisible = false
+                }
+            }
+            /*binding.email.setText(email)*/
+
+            if(photoUrl != null){
+                Thread{
+                    val file = saveLocalFile(photoUrl.toString())
+                    runOnUiThread{
+                        val bitmap = BitmapFactory.decodeFile(file.path)
+                        binding.imgPerfil.setImageBitmap(bitmap)
+                    }
+                }.start()
+            }
+        }
+
         binding.logout.setOnClickListener{
             firebaseAuth.signOut()
             mGoogleSignClient.signOut()
@@ -62,11 +96,6 @@ class MainActivity : AppCompatActivity() {
 
         binding.profile.setOnClickListener{
             val activity = Intent(this, ProfileActivity::class.java);
-            startActivity(activity)
-        }
-
-        binding.fabAddTask.setOnClickListener{
-            val activity = Intent(this, LivrosActivity::class.java);
             startActivity(activity)
         }
 
@@ -91,5 +120,33 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(ctx, "Erro ao carregar tarefas", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    fun saveLocalFile(_url: String): File {
+        val url = URL(_url)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.doInput = true
+        connection.connect()
+
+        val input = connection.inputStream
+        val dir = File(getExternalFilesDir(null), "images")
+        if(!dir.exists()){
+            dir.mkdir()
+        }
+
+        val file = File(dir, "imagem.jpg")
+        val output = FileOutputStream(file)
+
+        val buffer = ByteArray(1024)
+        var read: Int;
+        while(input.read(buffer).also { read = it} != -1){
+            output.write(buffer, 0, read)
+        }
+
+        output.flush()
+        output.close()
+        input.close()
+
+        return file
     }
 }
